a647e99f5fdbae2e429de7e865b2d39c17bd79a8
Tarsis <br.tmvdl@gmail.com>
2023-12-26 11:57:01 -0300

    Updating app files

--- src/api/libs/router/index.js
+++ src/api/libs/router/index.js
@@ -1,5 +1,35 @@
 const Logger = require('../logger/')
 
+class Route {
+  method = 'GET'
+  path = new RegExp('')
+
+  setPath(path = new RegExp('')) {
+    this.path = new RegExp(path, 'ig')
+    return this
+  }
+
+  getPath() {
+    return this.path
+  }
+
+  setMethod(method) {
+    this.method = method
+    return this
+  }
+
+  getMethod() {
+    return this.method
+  }
+
+  isRequest(req) {
+    const isMethod = this.method === req.method
+    const isPath = this.path.test(req.path)
+    
+    return isMethod && isPath
+  }
+}
+
 class Router {
   log = new Logger('Router')
 
